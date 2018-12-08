7904576c81cd1e0897499ec2286c3202bbc454b1
eric010101 <pengyauwang@hotmail.com>
2018-12-08 13:00:30 +0800

    first commit

---
+++ config/my.php
@@ -0,0 +1,10 @@
+<?php
+$config->installed    = true;
+$config->debug        = false;
+$config->requestType  = 'GET';
+$config->db->host     = 'localhost';
+$config->db->port     = '3306';
+$config->db->name     = 'esbc_zdao01';
+$config->db->user     = 'ESBCadmin';
+$config->db->password = 'ESBChappy';
+$config->db->prefix   = 'zdao_';
---
+++ tmp/log/cron.20181208.log.php
@@ -0,0 +1,52 @@
+<?php
+ die();
+12:08:59 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
+12:09:59 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
+12:10:59 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
+12:10:59 task 6 executed,
+command: appName=sys&moduleName=queue&methodName=additional.
+return : .
+output : ok
+
+12:11:59 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
+12:12:59 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
+12:13:59 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
+12:14:59 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
+12:15:59 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
+12:16:59 task 5 executed,
+command: appName=sys&moduleName=queue&methodName=getqueue.
+return : .
+output : ok
+
